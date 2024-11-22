41da53b0c9dc9437538242c98ca7c57ffacfe52d
SeaReccoon <proslava50@gmail.com>
2024-11-22 06:27:31 +0300

    Добавлена модель Lesson

--- lessons/models.py
+++ lessons/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django_ckeditor_5.fields import CKEditor5Field
 
 class Lesson (models.Model):
     title = models.CharField("Название", max_length=150)
     date = models.DateField("Дата обновления", auto_now=True, auto_created=True)
+    content = CKEditor5Field("Содержимое")
+
+    def __str__(self):
+        return self.title
     
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
