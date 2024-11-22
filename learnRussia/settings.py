d4955f294361c6aec643de4426d7047633e7e88f
SeaReccoon <proslava50@gmail.com>
2024-11-22 06:12:27 +0300

    Перустановлено на ckeditor-5

--- learnRussia/settings.py
+++ learnRussia/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ckeditor',
+    'django_ckeditor_5',
     'main',
     'lessons'
 ]
