37fe4df47df9d4b7445732a9f39f7a2fea893b79
SeaReccoon <proslava50@gmail.com>
2024-11-24 04:10:18 +0300

    создание личного кабинета

--- user/views.py
+++ user/views.py
@@ -27,3 +27,15 @@ class AccountView(DetailView):
     model = User
     template_name = "user/account.html"
     context_object_name = 'user'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.get_object()
+        fields = {
+            "Логин": user.username,
+            "Имя": user.first_name,
+            "Фамилия": user.last_name,
+            "Почта": user.email
+        }
+        context["fields"] = fields
+        return context
