40e0ca0e2f790fe4837c62d73c26c60227baf321
SeaReccoon <proslava50@gmail.com>
2024-11-25 00:25:42 +0300

    добавлена страница обратной связи

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from main.views import IndexView
+from main.views import IndexView, FeedbackView, SuccessFeedback
 
 urlpatterns = [
+    path("feedback/success/", SuccessFeedback.as_view(), name="success_feedback"),
+    path("feedback/", FeedbackView.as_view(), name="feedback"),
     path("", IndexView.as_view(), name="index")
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,29 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from main.models import Feedback
+from django.urls import reverse
+from django.core.mail import send_mail
+from django.conf import settings
+
+def email(subject, content):
+    send_mail(subject, content, settings.EMAIL_HOST_USER, [settings.EMAIL_HOST_USER, ])
 
 class IndexView (TemplateView):
     template_name = "main/index.html"
 
+class FeedbackView (CreateView):
+    model = Feedback
+    template_name = "main/feedback.html"
+    fields = ["text",]
+
+    def get_success_url(self):
+        return reverse("success_feedback")
+    
+    def form_valid(self, form):
+        user = self.request.user
+        form.instance.user = user
+        subject = "Обратная связь от " + user.username
+        email(subject, form.data["text"])
+        return super().form_valid(form)
+
+class SuccessFeedback (TemplateView):
+    template_name = "main/success_feedback.html"
