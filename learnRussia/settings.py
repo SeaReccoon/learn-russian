5d90092a0cdfea7506aa792d16041b83db6ca693
SeaReccoon <proslava50@gmail.com>
2024-11-25 06:49:29 +0300

    настройка редактора ckeditor

--- learnRussia/settings.py
+++ learnRussia/settings.py
@@ -159,23 +159,11 @@ customColorPalette = [
 
 CKEDITOR_5_CONFIGS = {
     'default': {
-        'toolbar': ['heading', '|', 'bold', 'italic', 'link',
-                    'bulletedList', 'numberedList', 'blockQuote', 'imageUpload', ],
-
-    },
-    'extends': {
-        'blockToolbar': [
-            'paragraph', 'heading1', 'heading2', 'heading3',
-            '|',
-            'bulletedList', 'numberedList',
-            '|',
-            'blockQuote',
-        ],
-        'toolbar': ['heading', '|', 'outdent', 'indent', '|', 'bold', 'italic', 'link', 'underline', 'strikethrough',
-        'code','subscript', 'superscript', 'highlight', '|', 'codeBlock', 'sourceEditing', 'insertImage',
-                    'bulletedList', 'numberedList', 'todoList', '|',  'blockQuote', 'imageUpload', '|',
-                    'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', 'mediaEmbed', 'removeFormat',
-                    'insertTable',],
+        'toolbar': [
+                    'undo', 'redo', '|', 'heading', '|',
+                    'outdent', 'indent', '|', 'bold', 'italic', 'link', 'underline', 'strikethrough', 'highlight', '|',
+                    'insertImage', 'bulletedList', 'numberedList',  'blockQuote', '|',
+                    'fontSize', 'fontFamily', 'fontColor', 'insertTable',],
         'image': {
             'toolbar': ['imageTextAlternative', '|', 'imageStyle:alignLeft',
                         'imageStyle:alignRight', 'imageStyle:alignCenter', 'imageStyle:side',  '|'],
@@ -202,18 +190,10 @@ CKEDITOR_5_CONFIGS = {
         },
         'heading' : {
             'options': [
-                { 'model': 'paragraph', 'title': 'Paragraph', 'class': 'ck-heading_paragraph' },
-                { 'model': 'heading1', 'view': 'h1', 'title': 'Heading 1', 'class': 'ck-heading_heading1' },
-                { 'model': 'heading2', 'view': 'h2', 'title': 'Heading 2', 'class': 'ck-heading_heading2' },
-                { 'model': 'heading3', 'view': 'h3', 'title': 'Heading 3', 'class': 'ck-heading_heading3' }
+                { 'model': 'paragraph', 'title': 'Текст', 'class': 'text' },
+                { 'model': 'heading1', 'view': 'h2', 'title': 'Заголовок', 'class': 'title' },
+                { 'model': 'heading2', 'view': 'h3', 'title': 'Подзаголовок', 'class': 'sub-title' }
             ]
-        }
-    },
-    'list': {
-        'properties': {
-            'styles': 'true',
-            'startIndex': 'true',
-            'reversed': 'true',
         }
     }
 }
