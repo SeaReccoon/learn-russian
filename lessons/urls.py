08865dfbc0e6e1dbe293f9122a482cafc3509032
SeaReccoon <proslava50@gmail.com>
2024-11-26 09:49:05 +0300

    добавлена страница для отображения уроков

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from lessons.views import ListLessonsView
+from lessons.views import ListLessonsView, LessonView
 
 urlpatterns = [
+    path("lesson/<int:pk>", LessonView.as_view(), name="lesson"),
     path("list/", ListLessonsView.as_view(), name="lessons_list")
 ]
--- lessons/views.py
+++ lessons/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from lessons.models import Lesson
 
 class ListLessonsView(ListView):
@@ -15,3 +15,14 @@ class ListLessonsView(ListView):
                 categories[category] = []
             categories[category].append(lesson)
         return categories
+
+class LessonView(DetailView):
+    model = Lesson
+    template_name = "lessons/lesson.html"
+    context_object_name = "lesson"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        obj = self.get_object()
+        context["other_lessons"] = Lesson.objects.filter(category=obj.category)
+        return context
