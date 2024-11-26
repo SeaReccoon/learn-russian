fff16bfd455e866d87f4f2935f21952f076b6027
SeaReccoon <proslava50@gmail.com>
2024-11-26 08:07:53 +0300

    добавлена страница содержания

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from lessons.views import ListLessonsView
 
 urlpatterns = [
-    
+    path("list/", ListLessonsView.as_view(), name="lessons_list")
 ]
--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from lessons.models import Lesson
 
-# Create your views here.
+class ListLessonsView(ListView):
+    model = Lesson
+    template_name = "lessons/list.html"
+    context_object_name = "categories"
+
+    def get_queryset(self):
+        categories = {}
+        lessons = Lesson.objects.all()
+        for lesson in lessons:
+            category = lesson.category
+            if not category in categories:
+                categories[category] = []
+            categories[category].append(lesson)
+        return categories
