e90a0b606a74575d4fa8a27c381d4dbd44009e09
SeaReccoon <proslava50@gmail.com>
2024-11-26 08:05:00 +0300

    фикс сортировки уроков

--- lessons/models.py
+++ lessons/models.py
@@ -14,4 +14,4 @@ class Lesson (models.Model):
     class Meta:
         verbose_name = "Урок"
         verbose_name_plural = "Уроки"
-        ordering =["number", "category"]
+        ordering =["category", "number"]
