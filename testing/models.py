a2bf916c933cdb8d9a101566fe54482048f81b7c
SeaReccoon <proslava50@gmail.com>
2024-11-29 06:17:48 +0300

    созданы модели для тестов

--- testing/models.py
+++ testing/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question = models.CharField("Вопрос", max_length=200)
+    weight = models.IntegerField("Вес вопроса", default=2)
+
+    def __str__(self):
+        return self.question
+    
+    class Meta:
+        verbose_name = "Вопрос"
+        verbose_name_plural = "Вопросы"
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="Вопрос", related_name="questions")
+    answer = models.CharField("Ответ", max_length=150)
+    is_correct = models.BooleanField("Правильный ответ", default=False)
+
+    def __str__(self):
+        return f"{self.question.question}: {self.answer}"
+    
+    class Meta:
+        verbose_name = "Ответ"
+        verbose_name_plural = "Ответы"
+    
