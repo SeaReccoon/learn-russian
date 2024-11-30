210a351966800dfff2f4e2a7c81aa06869c0d94f
SeaReccoon <proslava50@gmail.com>
2024-11-30 09:30:22 +0300

    Добавлены страницы с тестами знаний

--- testing/urls.py
+++ testing/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from testing.views import QuestionsListView, StartTestingView
 
 urlpatterns = [
-   
+    path("test/", QuestionsListView.as_view(), name="testing"),
+    path("", StartTestingView.as_view(), name="start_test")
 ]
--- testing/views.py
+++ testing/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
+from testing.models import Question
+import random
 
-# Create your views here.
+class QuestionsListView(ListView):
+    model = Question
+    template_name = "testing/test.html"
+    context_object_name = "questions"
+
+    def get_queryset(self):
+        questions = Question.objects.all()
+        random.shuffle(list(questions))
+        return questions
+
+class StartTestingView(TemplateView):
+    template_name = "testing/start.html"
